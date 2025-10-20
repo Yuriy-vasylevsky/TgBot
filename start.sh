88e45c3013c10753dc3670dcbb115ef4f745cc44
рій асилевський <yuriy.vasylevsky@gmail.com>
2025-10-20 20:24:55 +0300

    3

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
-# #!/bin/bash
-# echo "🚀 Starting Telegram Bot..."
+#!/bin/bash
+echo "🚀 Starting Telegram Bot..."
 
-# # Активуємо середовище Railway
-# source /app/.venv/bin/activate
+# Активуємо середовище Railway
+source /app/.venv/bin/activate
 
-# # Перевіримо, чи існує база
-# if [ ! -f "users.db" ]; then
-#   echo "📦 Створюємо нову базу users.db..."
-#   touch users.db
-# fi
+# Перевіримо, чи існує база
+if [ ! -f "users.db" ]; then
+  echo "📦 Створюємо нову базу users.db..."
+  touch users.db
+fi
 
-# # Запуск
-# python main.py
+# Запуск
+python main.py
 
-web: bash start.sh
+# web: bash start.sh
