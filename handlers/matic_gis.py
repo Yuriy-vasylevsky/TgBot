54c0107997281adabfbfdaa97258a2b9a91041b6
Yuriy-vasylevsky <yuriy.vasylevsky@gmail.com>
2026-06-21 16:40:24 +0300

    1

--- handlers/matic_gis.py
+++ handlers/matic_gis.py
@@ -24,7 +24,7 @@ GIS_API_URL = "https://api.superplat.pw/api/gisv2/"
 GIS_INIT_SESSION_URL = GIS_API_URL + "init.session"
 GIS_CLOSE_SESSION_URL = GIS_API_URL + "close.session"
 
-GIS_GAME_ID = 2133
+GIS_GAME_ID = 20
 GIS_DEFAULT_CURRENCY = "UAH"
 
 
