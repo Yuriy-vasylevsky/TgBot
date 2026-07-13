4764752e2be91cb0248b67b8d6004b7d897a5280
Yuriy-vasylevsky <yuriy.vasylevsky@gmail.com>
2026-07-13 17:51:29 +0300

    1

--- main.py
+++ main.py
@@ -343,15 +343,51 @@ async def cancel_reset_gifts(callback: types.CallbackQuery):
 # ==========================
 # КОМАНДИ ТА МЕНЮ
 # ==========================
+# async def set_commands():
+#     await bot.set_my_commands(
+#         [BotCommand(command="start", description="🔄 Рестарт")],
+#         scope=BotCommandScopeAllPrivateChats()
+#     )
+#     await bot.delete_my_commands(scope=BotCommandScopeAllGroupChats())
+
+#     admin_commands = [
+#         BotCommand(command="safe", description="🔒 Показати Сейф"),
+#         BotCommand(command="open", description="🔓 Відкрити клітинку"),
+#         BotCommand(command="bowling", description="🎳 Боулінг"),
+#         BotCommand(command="basketball", description="🏀 Баскетбол"),
+#         BotCommand(command="football", description="⚽ Футбол"),
+#         BotCommand(command="wordle", description="🎭 Вгадай слово"),
+#         BotCommand(command="numbers", description="🕵️‍♂️ Вгадай код"),
+#         BotCommand(command="jackpot2", description="💵💵 Jackpot"),
+#         BotCommand(command="jackpot", description="💵💵💵 Jackpot"),
+#         BotCommand(command="jackpot5", description="💵💵💵💵💵 Jackpot"),
+#         BotCommand(command="skarb", description="💎 Найди скарб"),
+#         BotCommand(command="/vote_prize", description="Голосування"),
+#         BotCommand(command="/minefield", description="Промо борьба"),
+#         BotCommand(command="/bank", description="Банк "),
+#     ]
+#     await bot.set_my_commands(
+#         commands=admin_commands,
+#         scope=BotCommandScopeAllChatAdministrators()
+#     )
+
 async def set_commands():
     await bot.set_my_commands(
         [BotCommand(command="start", description="🔄 Рестарт")],
         scope=BotCommandScopeAllPrivateChats()
     )
-    await bot.delete_my_commands(scope=BotCommandScopeAllGroupChats())
+
+    # Команди для ВСІХ учасників груп (не тільки адмінів)
+    group_commands = [
+        BotCommand(command="safe", description="🔒 Показати Сейф"),
+    ]
+    await bot.set_my_commands(
+        commands=group_commands,
+        scope=BotCommandScopeAllGroupChats()
+    )
 
     admin_commands = [
-        BotCommand(command="safe", description="🔒 Показати Сейф"),
+        BotCommand(command="safe", description="🔒 Показати Сейф"),  # дублюємо, щоб адміни теж бачили
         BotCommand(command="open", description="🔓 Відкрити клітинку"),
         BotCommand(command="bowling", description="🎳 Боулінг"),
         BotCommand(command="basketball", description="🏀 Баскетбол"),
@@ -371,7 +407,7 @@ async def set_commands():
         scope=BotCommandScopeAllChatAdministrators()
     )
 
-
+    
 import asyncio
 from db import cleanup_old_payment_logs
 from handlers.casino_api import _matic_api as matic_api
